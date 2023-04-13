4e4cf85d85c8e60953cfdaba099f22a4af6ada48
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-04-13 17:04:42 +0200

    Update localStorage.js

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,12 +1,31 @@
-const STORAGE_KEY_WATCH ='save-watch';
-let arrFilms = [];
+const STORAGE_KEY_WATCH = 'save-watch';
+const STORAGE_KEY_QUEUE = 'save-queue';
+let arrFilmsWatch = [];
+let arrFilmQueue = [];
+
 
 export const saveLocalStorageWatch = (film) => {
    
-    if (arrFilms.find(films=> films.id === film.id)) {
+    if (arrFilmsWatch.find(films => films.id === film.id)) {
         console.log('Однаковий id');
         return;
     }
-    arrFilms.push(film);
-    localStorage.setItem(STORAGE_KEY_WATCH, JSON.stringify(arrFilms));
+    arrFilmsWatch.push(film);
+    localStorage.setItem(STORAGE_KEY_WATCH, JSON.stringify(arrFilmsWatch));
+};
+
+export const saveLocalStorageQueue = (film) => {
+    if (arrFilmQueue.find(films => films.id === film.id)) {
+        console.log('Однаковий id');
+        return;
+    }
+    arrFilmQueue.push(film);
+    localStorage.setItem(STORAGE_KEY_QUEUE, JSON.stringify(arrFilmQueue));
+};
+
+const renderFilmWatch = () => {
+    const savedWatchFilm = JSON.parse(localStorage.getItem(STORAGE_KEY_WATCH));
+
 }
+
+//${BASE_URL}/search/${media_type}?api_key=0b0c22042155193a7073ae89a4562c2a&language=en-US&query=${name}
