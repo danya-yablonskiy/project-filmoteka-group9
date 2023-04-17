10890378aad599c8eca6e6a8ff46d93ec06af4e2
Valentine Pylypchuk <strheadshot1997@gmail.com>
2023-04-17 21:27:46 +0300

    add work files
    +added render cards on load pages
    +added pagination for keyword search and on load main page
    +added div block at main.html for pagination
    +added import styles for gallery to index.scss
    +Update renderMarkupCards.js
    +Update fetchKeyword.js and fetchPopularMovies.js

--- src/js/fetchPopularMovies.js
+++ src/js/fetchPopularMovies.js
@@ -8,7 +8,7 @@ export default async function fetchPopularMovies(page) {
   const { BASE_URL, API_KEY, media_type, time_window} = pathParams;
   try {
     const response = await
-          axios.get(`${BASE_URL}/trending/${media_type}/${time_window}?api_key=${API_KEY}&page=${page}`);
+      axios.get(`${BASE_URL}/trending/${media_type}/${time_window}?api_key=${API_KEY}&page=${page}`);
     return response.data;
   } catch (error) {
     console.error(error);
