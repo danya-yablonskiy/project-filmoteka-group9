93eff752041509787d7a19e8541b80f74fcbd71d
Maria Vovchuk <mari4ka62@gmail.com>
2023-04-18 12:48:07 +0300

    add function from localStorage

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,17 @@
-// import { displayLoader } from './loader';
-// import { hideLoader } from './loader';
-// import { renderFilmLibrary } from './localStorage';
-// import { appendMarkup } from './renderMarkupCards';
+import { clearMarkup } from './pagination';
+import displayLoader  from "./loader";
+import hideLoader from "./loader";
+import renderFilmLibrary from './localStorage';
 
-const logo = document.querySelector('.header-logo');
+ 
 const form = document.querySelector('.search-form');
-const header = document.querySelector('.header-container');
+ 
 const btnHome = document.querySelector('.button-home');
 const btnLibrary = document.querySelector('.button-library');
-const input = document.querySelector('.search-form__input');
-const libraryBox = document.querySelector('.library-button-box');
 const logInBtn = document.querySelector('.login-button');
-const gallery = document.querySelector('.gallery');
+const libraryBox = document.querySelector('.library-button-box');
+
+ 
 
 
 
@@ -30,16 +30,18 @@ function onBtnLibraryClick(e) {
     currentPage.classList.add('is-active');
     
 //запустити функцію,яка очищує контейнер
-    // gellaryClear();
+    clearMarkup();
  
     form.classList.add('is-hidden');
     logInBtn.classList.add('is-hidden');
     libraryBox.classList.remove('is-hidden');
 
     // запустити лоадер
-    // displayLoader();
-    // hideLoader();
+    displayLoader();
+    hideLoader();
+     
     // //запустити рендер функції локал сторейдж
+    renderFilmLibrary();
      
     
 }
@@ -53,8 +55,6 @@ function onBtnHomeClick(e) {
     logInBtn.classList.remove('is-hidden');
     libraryBox.classList.add('is-hidden');
 
-    // appendMarkup();
+    
 }
-function gellaryClear(){
-    gallery.innerHTML = '';
-}
+ 
