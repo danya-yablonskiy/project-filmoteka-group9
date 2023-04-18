3f0fa3f358343b6b66ee7e3fac04812d96010912
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-04-18 07:27:50 +0200

    Update localStorage.js

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -9,6 +9,13 @@ const STORAGE_KEY_QUEUE = 'save-queue';
 // button1.addEventListener('click', saveLocalStorageWatch);
 // button2.addEventListener('click', saveLocalStorageQueue);
 
+//data-action="watch"
+//data-action="queue"
+//data-action="library"
+
+//const watchBtn = document.querySelector('button[data-action="watch"]');
+//const queueBtn = document.querySelector('button[data-action="queue"]');
+//const libraryBtn = document.querySelector('button[data-action="library"]');
 
 
 const saveLocalStorageWatch = (event) => {
@@ -45,8 +52,7 @@ const saveLocalStorageQueue = (event) => {
         genre_ids: buttonEl.dataset.genre,
         poster_path: buttonEl.dataset.poster,
         release_date:buttonEl.dataset.release,
-    };
-    // console.log(film)
+    };   
     if (localStorage.getItem(STORAGE_KEY_QUEUE)) {
         arrFilmQueue = JSON.parse(localStorage.getItem(STORAGE_KEY_QUEUE));
         if (arrFilmQueue.find(filmQueue => filmQueue.id === film.id)) {             
@@ -67,8 +73,10 @@ const renderFilmWatch = () => {
     //
     if (localStorage.getItem(STORAGE_KEY_WATCH)) {
         const savedFilm = JSON.parse(localStorage.getItem(STORAGE_KEY_WATCH));
-        //appendMarkup(savedFilm);
+        appendMarkup(savedFilm);
+        return;
     }
+    Notiflix.Notify.failure('Not Found saved Watch');
 };
 
 const renderFilmQueue = () => {
@@ -77,8 +85,10 @@ const renderFilmQueue = () => {
     //
     if (localStorage.getItem(STORAGE_KEY_QUEUE)) {
         const savedFilm = JSON.parse(localStorage.getItem(STORAGE_KEY_QUEUE));
-        //appendMarkup(savedFilm);
+        appendMarkup(savedFilm);
+        return;
     }
+    Notiflix.Notify.failure('Not Found saved Queue');
 };
 
 const renderFilmLibrary = () => {
@@ -102,7 +112,7 @@ const renderFilmLibrary = () => {
     }
     if (!savedFilmWatch) {
         if (!savedFilmQueue) {
-            Notiflix.Notify.failure('no movie saved');
+            Notiflix.Notify.failure('Not Found movie saved');
             return;
         }
         appendMarkup(savedFilmQueue);
@@ -112,13 +122,13 @@ const renderFilmLibrary = () => {
 
 }
     
-const deleteFilmInWatch = (event) => {
+const deleteFilmInWatch = (event) => {    
     let arrSevedFilms = [];
     const buttonEl = event.currentTarget;
     if (localStorage.getItem(STORAGE_KEY_WATCH)) {
         arrSevedFilms = JSON.parse(localStorage.getItem(STORAGE_KEY_WATCH));
         const indexFilm = arrSevedFilms.findIndex(film => film.id === buttonEl.dataset.id);        
-        if (indexFilm < 0) {
+        if (indexFilm < 0) {            
             Notiflix.Notify.failure('Такого фільма немає у Watch');
             return;
         }
