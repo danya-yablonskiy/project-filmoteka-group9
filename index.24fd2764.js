42233fd4092838c597eeedf075408d551384006b
danya-yablonskiy <115042288+danya-yablonskiy@users.noreply.github.com>
2023-04-18 10:46:28 +0000

    Deploying to gh-pages from @ danya-yablonskiy/project-filmoteka-group9@1438b50b9907525b73b846a48c457bb2ab5c7a5d 🚀

--- index.24fd2764.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".login-button"),d=document.querySelector("[data-modal-header]"),t=document.querySelector("[data-modal-header__close]");e.addEventListener("click",(function(e){d.classList.remove("is-hidden")})),t.addEventListener("click",(function(e){d.classList.add("is-hidden")}));
-//# sourceMappingURL=index.24fd2764.js.map
