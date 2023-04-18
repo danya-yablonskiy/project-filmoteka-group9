bd8c0bdac90f2c693108b95b6ef56b2641a4d5cd
Maria Vovchuk <mari4ka62@gmail.com>
2023-04-18 19:04:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ export const refs = {
   watchBtn : document.querySelector('button[data-action="watch"]'),
   queueBtn : document.querySelector('button[data-action="queue"]'),
   libraryBtn: document.querySelector('button[data-action="library"]'),
-  
 };
 
 refs.filmCardsContainer.addEventListener('click', onFilmCardsContainerClick);
