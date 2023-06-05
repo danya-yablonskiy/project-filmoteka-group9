88ee9ff30d410786b7134e86003c2c59e0fa9813
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-06 01:09:56 +0300

    Update options.js

--- src/js/options.js
+++ src/js/options.js
@@ -1,9 +1,8 @@
-const API_KEY = '0b0c22042155193a7073ae89a4562c2a';
+const API_KEY = 'bbc8a1fc1c52d42637b9f4c38cf0115f';
 
 export const pathParams = {
   BASE_URL: 'https://api.themoviedb.org/3/',
-  API_KEY: '0b0c22042155193a7073ae89a4562c2a',
+  API_KEY: 'bbc8a1fc1c52d42637b9f4c38cf0115f',
   media_type: 'movie',
   time_window: 'week',
-
-}
+};
